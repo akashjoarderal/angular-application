e5121a84c064cfb5ef27b02113d593bfb779f38c
sayed al momin <akash09joarder@yahoo.com>
2018-08-05 22:41:58 +0600

    angular test application

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>hello everyone</h1><h3>hello you</h3>',
+  styles:['h1{color:red;}h3{color:yellow;}']
+  
+})
+export class AppComponent {
+  title = 'angular-app';
+}
